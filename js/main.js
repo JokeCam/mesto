e0693d2929be145a4566beaae5826f13b444b2a3
Stanislav Selunin <infleshlightwetrust@gmail.com>
2020-12-07 15:54:52 +0300

    feat: added delete_button

--- js/main.js
+++ js/main.js
@@ -3,6 +3,33 @@ const buttonEdit = document.querySelector('.profile__button');
 const buttonClose = popup.querySelector('.popup__close');
 const formElement = popup.querySelector('.popup__form');
 
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 let nameInput = formElement.querySelector('#first-name');
 let jobInput = formElement.querySelector('#job-name');
 
@@ -13,7 +40,7 @@ function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = firstName.textContent;
   jobInput.value = jobName.textContent;
-}
+} //function opens popup and delivers current page value to inputs
 
 function closePopup() {
   popup.classList.remove('popup_opened');
@@ -26,7 +53,7 @@ function formSubmitHandler (evt) {
     jobName.textContent = jobInput.value;
 
     closePopup()
-}
+} //function sends input values back to page and closes popup
 
 buttonEdit.addEventListener('click', openPopup);
 buttonClose.addEventListener('click', closePopup);
